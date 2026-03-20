27dc303f4fadd2d9bd172e965255f5901c1e63c7
Zumwaltboi68_mirror <alexanderpiguet66@gmail.com>
2026-03-20 14:42:05 +0000

    Build and optimize for GitHub Pages

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,26 @@
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
-
 const LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const webpack = require('webpack')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
+  resolve: {
+    fallback: {
+      "vm": require.resolve("vm-browserify"),
+      "canvas": false // Prevents build failure from missing native canvas libs
+    }
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        parallel: false, // CRITICAL: Stops memory-hungry worker processes
+      }),
+    ],
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new webpack.optimize.ModuleConcatenationPlugin(),
